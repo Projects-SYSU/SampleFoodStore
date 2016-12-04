51e58848d7a821bef8a701ac833276930ec221cc
shower666 <ly6513@gmail.com>
2016-12-04 15:33:41 +0800

    finish

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Sample Food Store
 
-原型设计程序（无数据库版本、无权限）
-
 运行`mvn tomcat7:run`
 
 提交前记得`mvn clean`
--- sfs/src/main/java/thymeleafexamples/sfs/business/entities/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package thymeleafexamples.sfs.business.entities.repositories;
-
-/**
- * Created by shower on 16-11-7.
- */
-public class CustomerRepository {
-}
